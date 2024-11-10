0f886e27d4932aa8eb7fa246d2a931465da2b7ce
3022232100-Li Guohong <singercivilization@163.com>
2024-11-10 19:12:58 +0800

    test sll

---
+++ TEMU/mips_sc/build/shiftR.asm
@@ -0,0 +1,16 @@
+
+build/shiftR:     file format elf32-tradlittlemips
+build/shiftR
+
+
+Disassembly of section .text:
+
+80000000 <main>:
+80000000:	00022080 	sll	a0,v0,0x2
+80000004:	4a000000 	c2	0x0
+
+Disassembly of section .reginfo:
+
+00000000 <.reginfo>:
+   0:	00000014 	0x14
+	...
