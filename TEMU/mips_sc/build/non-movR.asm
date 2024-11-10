6555d22d79a38a8b6a5756e51ed05a979fce4fef
3022232100-Li Guohong <singercivilization@163.com>
2024-11-10 18:29:30 +0800

    test non-movR

--- TEMU/mips_sc/build/non-movR.asm
+++ TEMU/mips_sc/build/non-movR.asm
@@ -1,6 +1,6 @@
 
-build/rlogic:     file format elf32-tradlittlemips
-build/rlogic
+build/non-movR:     file format elf32-tradlittlemips
+build/non-movR
 
 
 Disassembly of section .text:
@@ -8,10 +8,11 @@ Disassembly of section .text:
 80000000 <main>:
 80000000:	00412024 	and	a0,v0,at
 80000004:	00412825 	or	a1,v0,at
-80000008:	4a000000 	c2	0x0
+80000008:	00413021 	addu	a2,v0,at
+8000000c:	4a000000 	c2	0x0
 
 Disassembly of section .reginfo:
 
 00000000 <.reginfo>:
-   0:	00000036 	0x36
+   0:	00000076 	0x76
 	...
