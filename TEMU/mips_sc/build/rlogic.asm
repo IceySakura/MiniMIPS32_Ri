6c0853a2e8ce966d961849cb08b01560a26bc6ce
3022232100-Li Guohong <singercivilization@163.com>
2024-11-10 17:28:27 +0800

    test rlogic

---
+++ TEMU/mips_sc/build/rlogic.asm
@@ -0,0 +1,17 @@
+
+build/rlogic:     file format elf32-tradlittlemips
+build/rlogic
+
+
+Disassembly of section .text:
+
+80000000 <main>:
+80000000:	00412024 	and	a0,v0,at
+80000004:	00412825 	or	a1,v0,at
+80000008:	4a000000 	c2	0x0
+
+Disassembly of section .reginfo:
+
+00000000 <.reginfo>:
+   0:	00000036 	0x36
+	...
